fc3a9b7e865440113a9d44be5d61784815ddb116
Jealous <CooLanfei@163.com>
2018-01-06 14:54:12 +0100

    Add bulleye detection mechanism

--- wechat-jump.js
+++ wechat-jump.js
@@ -1,3 +1,5 @@
+var piece;
+var bullseye;
 var deviceWidth = device.width;
 var deviceHeight = device.height;
 
@@ -12,36 +14,47 @@ function isDifferentColor(colorA, colorB, threshold) {
 }
 
 // 查找落点横坐标
-function findBoardX(screen, pieceX) {
+function findBoardX(screen, pieceX, pieceWidth) {
+	var x;
+	var y;
+	var minX;
+	var maxX;
 	var borderX;
 	var borderY;
 	var borderPixelCount = 0;
 	var minY = parseInt(deviceHeight / 3);
 	var maxY = parseInt(deviceHeight / 3 * 2);
-	outer: for (var y = minY; y < maxY; y++) {
-		var minX;
-		var maxX;
+	var minPieceX = pieceX - pieceWidth / 2;
+	var maxPieceX = pieceX + pieceWidth / 2;
+	// 通过棋子位置判断在屏幕左半部分还是右半部分查找，提升性能
+	if (pieceX > deviceWidth / 2) {
+		minX = 0;
+		maxX = deviceWidth / 5 * 3;
+	} else {
+		minX = deviceWidth / 5 * 2;
+		maxX = deviceWidth;
+	}
+	if (bullseye) {
+		var bullseyePos = images.findImageInRegion(screen, bullseye, minX, minY, maxX - minX, maxY - minY);
+		if (bullseyePos) {
+			return bullseyePos.x + bullseye.getWidth() / 2;
+		}
+	}
+	outer: for (y = minY; y < maxY; y++) {
 		var firstPixel = screen.pixel(0, y);
-		// 通过棋子位置判断在屏幕左半部分还是右半部分查找，提升性能
-		if (pieceX > deviceWidth / 2) {
-			minX = 0;
-			maxX = deviceWidth / 2;
-		} else {
-			minX = deviceWidth / 2;
-			maxX = deviceWidth;
-		}
 		// 找出边缘像素点
-		for (var x = minX; x < maxX; ++x) {
+		for (x = minX; x < maxX; ++x) {
 			if (isDifferentColor(screen.pixel(x, y), firstPixel, 10)) {
-				if (!borderX) {
-					borderX = x;
-					borderY = y;
+				if (x < minPieceX || x > maxPieceX) {
+					if (!borderX) {
+						borderX = x;
+						borderY = y;
+					}
+					++borderPixelCount;
 				}
-				++borderPixelCount;
+			} else if (borderPixelCount > 0) {
+				break outer;
 			}
-		}
-		if (borderPixelCount > 0) {
-			break outer;
 		}
 	}
 	// 求出边缘中心点横坐标
@@ -49,34 +62,36 @@ function findBoardX(screen, pieceX) {
 }
 
 // 模拟长按跳跃
-function jump(distanceX) {
+function jump(duration) {
 	var x = random(0, deviceWidth);
 	var y = random(deviceHeight / 3, deviceHeight / 3 * 2);
-	press(x, y, distanceX * 1.6);
+	press(x, y, duration);
 }
 
 function keepAwake() {
 	device.keepScreenOn(3600000);
 	events.on('exit', function() {
-		toast('自动跳一跳结束');
+		toastLog('自动跳一跳结束');
 		device.cancelKeepingAwake();
 	});
 }
 
 function main() {
-	toast('正在加载自动跳一跳资源');
-	var piece = images.load('https://gitee.com/lanfei/wechat-jump/raw/master/piece.jpg');
+	toastLog('正在加载自动跳一跳资源');
+	piece = images.load('https://gitee.com/lanfei/wechat-jump/raw/master/piece.jpg');
+	// 是否启用靶心识别
+	bullseye = images.load('https://gitee.com/lanfei/wechat-jump/raw/master/bullseye.jpg');
 	if (!piece) {
-		toast('加载失败，请检查网络');
+		toastLog('加载失败，请检查网络');
 		return;
 	}
 	var pieceWidth = piece.getWidth();
-	var pieceHeight = piece.getHeight();
 	keepAwake();
+	toastLog('请允许程序进行屏幕截图');
 	images.requestScreenCapture();
-	toast('加载成功，请开始跳一跳游戏');
+	toastLog('加载成功，请开始跳一跳游戏');
 	waitForPackage('com.tencent.mm');
-	// console.log('Resolution:', deviceWidth, deviceHeight);
+	console.log('屏幕分辨率：', deviceWidth, deviceHeight);
 	while (currentPackage() == 'com.tencent.mm') {
 		var screen = images.captureScreen();
 		var pos = images.findImage(screen, piece, {
@@ -84,10 +99,11 @@ function main() {
 		});
 		if (pos) {
 			var pieceX = pos.x + pieceWidth / 2;
-			var boardX = findBoardX(screen, pieceX);
+			var boardX = findBoardX(screen, pieceX, pieceWidth);
+			var duration = Math.abs(pieceX - boardX) * 1.6;
 			console.log(pieceX, boardX);
-			jump(Math.abs(pieceX - boardX));
-			sleep(1500);
+			jump(duration);
+			sleep(1800);
 		}
 	}
 }
